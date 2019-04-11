440b9c2d3307c64b58606741617e66245c698c0d
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-11 13:13:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,8 @@ name_hash = {
 }
 
 def key_for_min_value(name_hash)
-  name_hash.each { |name, total | }
-  name_hash.sort_by { |total, name | }
+  new_hash = name_hash.each { |name, total | }
+  new_hash.sort_by { |total, name | }
 end
 
 puts key_for_min_value(name_hash)
